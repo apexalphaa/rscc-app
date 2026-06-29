e8e6e79123eb25223df98f5a9a66a37009398a39
Ankit Sharma <162314034+apexalphaa@users.noreply.github.com>
2026-06-29 19:07:04 +0530

    Update typography.js

--- apps/frontend/src/theme/typography.js
+++ apps/frontend/src/theme/typography.js
@@ -1,1 +1,18 @@
+const typography = {
+  heading: {
+    fontSize: "3rem",
+    fontWeight: "700",
+  },
 
+  subHeading: {
+    fontSize: "2rem",
+    fontWeight: "600",
+  },
+
+  body: {
+    fontSize: "1rem",
+    fontWeight: "400",
+  },
+};
+
+export default typography;
