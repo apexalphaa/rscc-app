0f41321fe73ebee0a034dd9112ba8de08906875a
Ankit Sharma <162314034+apexalphaa@users.noreply.github.com>
2026-06-30 15:29:18 +0530

    Update Home.jsx

--- apps/frontend/src/pages/Home.jsx
+++ apps/frontend/src/pages/Home.jsx
@@ -5,17 +5,25 @@ import Features from "../components/Features";
 import About from "../components/About";
 import Gallery from "../components/Gallery";
 import Footer from "../components/Footer";
+import WhyChoose from "../components/WhyChoose";
+import Coaches from "../components/Coaches";
+import UpcomingEvents from "../components/UpcomingEvents";
+import CTA from "../components/CTA";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <Hero />
-      <Stats />
-      <Features />
-      <About />
-      <Gallery />
-      <Footer />
-    </>
+  <Navbar />
+  <Hero />
+  <Stats />
+  <WhyChoose />
+  <Features />
+  <About />
+  <Coaches />
+  <UpcomingEvents />
+  <Gallery />
+  <CTA />
+  <Footer />
+</>
   );
 }
