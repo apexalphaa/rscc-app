b31f2b653e2d767ec1aac13ebcf19f0d59204b09
Ankit Sharma <162314034+apexalphaa@users.noreply.github.com>
2026-06-30 15:17:46 +0530

    Update Players.jsx

--- apps/frontend/src/pages/Players.jsx
+++ apps/frontend/src/pages/Players.jsx
@@ -1,13 +1,65 @@
+import Button from "../components/common/Button";
+import PageHeader from "../components/PageHeader";
+import SearchBar from "../components/SearchBar";
+import PlayerCard from "../components/PlayerCard";
+
+const players = [];
+
 export default function Players() {
   return (
-    <div className="p-10">
-      <h1 className="text-4xl font-bold">
-        Players
-      </h1>
+    <div className="p-8">
 
-      <p className="text-slate-500 mt-4">
-        Player management will be implemented in the next sprint.
-      </p>
+      <PageHeader
+        title="Players"
+        subtitle="Manage academy players"
+        action={
+          <Button>
+            + Add Player
+          </Button>
+        }
+      />
+
+      <div className="flex justify-between items-center mb-8">
+
+        <SearchBar
+          placeholder="Search player..."
+        />
+
+      </div>
+
+      {players.length === 0 ? (
+
+        <div className="bg-white rounded-2xl shadow-md p-16 text-center">
+
+          <h2 className="text-2xl font-bold">
+            No Players Yet
+          </h2>
+
+          <p className="text-slate-500 mt-3">
+
+            Click "Add Player" to register your first player.
+
+          </p>
+
+        </div>
+
+      ) : (
+
+        <div className="grid md:grid-cols-2 xl:grid-cols-4 gap-6">
+
+          {players.map((player) => (
+
+            <PlayerCard
+              key={player.id}
+              player={player}
+            />
+
+          ))}
+
+        </div>
+
+      )}
+
     </div>
   );
 }
