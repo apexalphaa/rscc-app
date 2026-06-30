46c9b37277ff82bbe6b70a7929a4a8e3b88cc4d5
Ankit Sharma <162314034+apexalphaa@users.noreply.github.com>
2026-06-30 15:11:33 +0530

    Update App.jsx

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router-dom";
+import Players from "./pages/Players";
+<Route path="/players" element={<Players />} />
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
