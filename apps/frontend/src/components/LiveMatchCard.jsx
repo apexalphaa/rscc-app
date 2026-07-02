7a560477b90d6964935ad91d7c1db9a7561ac16c
Ankit Sharma <162314034+apexalphaa@users.noreply.github.com>
2026-07-02 15:39:41 +0530

    Create LiveMatchCard.jsx

---
+++ apps/frontend/src/components/LiveMatchCard.jsx
@@ -0,0 +1,29 @@
+export default function LiveMatchCard(){
+
+    return(
+
+        <div className="bg-gradient-to-r from-red-500 to-red-600 rounded-3xl text-white p-8">
+
+            <p>
+
+                LIVE MATCH
+
+            </p>
+
+            <h2 className="text-4xl font-black mt-5">
+
+                No Live Match
+
+            </h2>
+
+            <p className="mt-5">
+
+                Start scoring to see live score.
+
+            </p>
+
+        </div>
+
+    )
+
+}
