4020f5da9ffbf4548382a395a0997015ce0bef5a
Ankit Sharma <162314034+apexalphaa@users.noreply.github.com>
2026-07-02 15:19:38 +0530

    Create AttendanceActions.jsx

---
+++ apps/frontend/src/components/AttendanceActions.jsx
@@ -0,0 +1,47 @@
+import Button from "./common/Button";
+
+export default function AttendanceActions({
+  players,
+  setAttendance,
+}) {
+
+  function markAllPresent() {
+
+    const obj = {};
+
+    players.forEach((player) => {
+      obj[player.id] = "Present";
+    });
+
+    setAttendance(obj);
+
+  }
+
+  function clearAttendance() {
+
+    setAttendance({});
+
+  }
+
+  return (
+
+    <div className="flex gap-4">
+
+      <Button
+        onClick={markAllPresent}
+      >
+        Mark All Present
+      </Button>
+
+      <Button
+        className="bg-slate-600"
+        onClick={clearAttendance}
+      >
+        Clear
+      </Button>
+
+    </div>
+
+  );
+
+}
