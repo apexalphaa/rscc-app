1927bcbdbb0b1cce3dedf9f78edf0ec613740df6
Ankit Sharma <162314034+apexalphaa@users.noreply.github.com>
2026-07-02 15:14:11 +0530

    Create attendanceDummyPlayers.js

---
+++ apps/frontend/src/data/attendanceDummyPlayers.js
@@ -0,0 +1,19 @@
+const attendanceDummyPlayers = [
+  {
+    id: 1,
+    name: "Rahul Sharma",
+    batch: "Under 16",
+  },
+  {
+    id: 2,
+    name: "Aryan Singh",
+    batch: "Under 16",
+  },
+  {
+    id: 3,
+    name: "Karan Patel",
+    batch: "Under 16",
+  },
+];
+
+export default attendanceDummyPlayers;
