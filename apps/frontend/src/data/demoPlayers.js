9234b45dca747b06b532192ccf24dde4b0718d05
Ankit Sharma <162314034+apexalphaa@users.noreply.github.com>
2026-07-03 13:26:56 +0530

    Create demoPlayers.js

---
+++ apps/frontend/src/data/demoPlayers.js
@@ -0,0 +1,15 @@
+const demoPlayers = [
+  "Rahul Sharma",
+  "Aryan Singh",
+  "Rohit Patel",
+  "Aman Verma",
+  "Harsh Yadav",
+  "Kunal Singh",
+  "Yash Gupta",
+  "Aditya Sharma",
+  "Ritesh Kumar",
+  "Vivek Patel",
+  "Saurabh Singh",
+];
+
+export default demoPlayers;
