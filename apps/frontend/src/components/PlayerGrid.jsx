2f5383b0cfaea8ea4b4754ae132de6aa6d1496f3
Ankit Sharma <162314034+apexalphaa@users.noreply.github.com>
2026-07-03 14:13:36 +0530

    Update PlayerGrid.jsx

--- apps/frontend/src/components/PlayerGrid.jsx
+++ apps/frontend/src/components/PlayerGrid.jsx
@@ -1,7 +1,23 @@
-export default function PlayerGrid() {
+import players from "../data/players";
+import PlayerCard from "./PlayerCard";
+
+export default function PlayersGrid() {
+
   return (
-    <div className="grid lg:grid-cols-3 gap-6">
+
+    <div className="grid md:grid-cols-2 xl:grid-cols-4 gap-6">
+
+      {players.map(player => (
+
+        <PlayerCard
+          key={player.id}
+          player={player}
+        />
+
+      ))}
 
     </div>
+
   );
+
 }
