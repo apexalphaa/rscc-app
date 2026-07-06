38465b7a6ef8ea7aae9582bff4a3e1da37dc9923
Ankit Sharma <162314034+apexalphaa@users.noreply.github.com>
2026-07-06 14:59:41 +0530

    Update database.js

--- apps/backend/config/database.js
+++ apps/backend/config/database.js
@@ -1,1 +1,27 @@
+import mongoose from "mongoose";
 
+const connectDatabase = async () => {
+
+    try {
+
+        const connection = await mongoose.connect(
+            process.env.MONGODB_URI
+        );
+
+        console.log(
+            `MongoDB Connected : ${connection.connection.host}`
+        );
+
+    }
+
+    catch (error) {
+
+        console.error(error.message);
+
+        process.exit(1);
+
+    }
+
+};
+
+export default connectDatabase;
