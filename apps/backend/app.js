940d9492a3c75a25d79ffdf2d513e00ea6016a88
Ankit Sharma <162314034+apexalphaa@users.noreply.github.com>
2026-07-06 15:00:10 +0530

    Update app.js

--- apps/backend/app.js
+++ apps/backend/app.js
@@ -1,1 +1,40 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
 
+const app = express();
+
+app.use(cors({
+
+    origin: process.env.CLIENT_URL,
+
+    credentials: true,
+
+}));
+
+app.use(express.json());
+
+app.use(express.urlencoded({
+
+    extended: true,
+
+}));
+
+app.use(cookieParser());
+
+app.use(morgan("dev"));
+
+app.get("/api/v1", (req, res) => {
+
+    res.json({
+
+        success: true,
+
+        message: "RSCC API Running"
+
+    });
+
+});
+
+export default app;
