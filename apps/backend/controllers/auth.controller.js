88d3689b71d2d55aa7a61fe9ab612cd2bc9b2169
Ankit Sharma <162314034+apexalphaa@users.noreply.github.com>
2026-07-06 18:25:36 +0530

    Update auth.controller.js

--- apps/backend/controllers/auth.controller.js
+++ apps/backend/controllers/auth.controller.js
@@ -1,1 +1,35 @@
+export const register = async (
+    req,
+    res
+) => {
 
+    res.json({
+        success: true,
+        message: "Register API Coming Next Sprint",
+    });
+
+};
+
+export const login = async (
+    req,
+    res
+) => {
+
+    res.json({
+        success: true,
+        message: "Login API Coming Next Sprint",
+    });
+
+};
+
+export const logout = async (
+    req,
+    res
+) => {
+
+    res.json({
+        success: true,
+        message: "Logout API Coming Next Sprint",
+    });
+
+};
