7138f68d167d4f8a898293ffbfb67ca3cd83b42a
Ankit Sharma <162314034+apexalphaa@users.noreply.github.com>
2026-07-07 15:12:33 +0530

    Update Team.js

--- apps/backend/models/Team.js
+++ apps/backend/models/Team.js
@@ -1,50 +1,79 @@
 import mongoose from "mongoose";
 
-const teamSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
+const teamSchema = new mongoose.Schema({
+
+    teamCode:{
+
+        type:String,
+
+        unique:true,
+
     },
 
-    shortName: {
-      type: String,
-      required: true,
+    name:{
+
+        type:String,
+
+        required:true,
+
+        unique:true,
+
     },
 
-    category: {
-      type: String,
-      enum: [
-        "U14",
-        "U16",
-        "U19",
-        "Senior",
-        "Visitor",
-      ],
+    shortName:{
+
+        type:String,
+
+        required:true,
+
     },
 
-    logo: String,
+    category:{
 
-    players: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Player",
-      },
-    ],
+        type:String,
 
-    coaches: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Coach",
-      },
-    ],
-  },
-  {
-    timestamps: true,
-  }
-);
+        enum:[
+            "U14",
+            "U16",
+            "U19",
+            "Senior",
+            "Visitor",
+        ],
+
+        required:true,
+
+    },
+
+    logo:{
+
+        type:String,
+
+        default:"",
+
+    },
+
+    players:[{
+
+        type:mongoose.Schema.Types.ObjectId,
+
+        ref:"Player",
+
+    }],
+
+    coaches:[{
+
+        type:mongoose.Schema.Types.ObjectId,
+
+        ref:"Coach",
+
+    }]
+
+},
+{
+    timestamps:true,
+});
 
 export default mongoose.model(
-  "Team",
-  teamSchema
+    "Team",
+    teamSchema
 );
