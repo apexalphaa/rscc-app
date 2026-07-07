6239e38955434f537377f50035a96bce0f9ec53e
Ankit Sharma <162314034+apexalphaa@users.noreply.github.com>
2026-07-07 13:57:50 +0530

    Update Team.js

--- apps/backend/models/Team.js
+++ apps/backend/models/Team.js
@@ -1,1 +1,50 @@
+import mongoose from "mongoose";
 
+const teamSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    shortName: {
+      type: String,
+      required: true,
+    },
+
+    category: {
+      type: String,
+      enum: [
+        "U14",
+        "U16",
+        "U19",
+        "Senior",
+        "Visitor",
+      ],
+    },
+
+    logo: String,
+
+    players: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Player",
+      },
+    ],
+
+    coaches: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Coach",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model(
+  "Team",
+  teamSchema
+);
