545c4c60601f3bcc5c534d62abc4652e804c8660
Ankit Sharma <162314034+apexalphaa@users.noreply.github.com>
2026-07-12 21:31:42 +0530

    Update catchAsync.js

--- apps/backend/utils/catchAsync.js
+++ apps/backend/utils/catchAsync.js
@@ -1,5 +1,13 @@
-module.exports = (fn) => {
-  return (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
+const catchAsync = (controller) => {
+
+    return (req, res, next) => {
+
+        Promise.resolve(
+            controller(req, res, next)
+        ).catch(next);
+
+    };
+
 };
+
+export default catchAsync;
