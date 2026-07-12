886a28d5ad08fbacfb8bb47bfa7b80726c3cc4d9
Ankit Sharma <162314034+apexalphaa@users.noreply.github.com>
2026-07-12 20:20:49 +0530

    Update ApiResponse.js

--- apps/backend/utils/ApiResponse.js
+++ apps/backend/utils/ApiResponse.js
@@ -1,1 +1,16 @@
+class ApiResponse {
+  constructor(
+    statusCode = 200,
+    message = "Success",
+    data = null,
+    success = true
+  ) {
+    this.success = success;
+    this.statusCode = statusCode;
+    this.message = message;
+    this.data = data;
+    this.timestamp = new Date().toISOString();
+  }
+}
 
+module.exports = ApiResponse;
