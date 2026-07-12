b5df5dd4e0068d95977594bcf75f0fdacc2d9570
Ankit Sharma <162314034+apexalphaa@users.noreply.github.com>
2026-07-12 20:28:58 +0530

    Create index.js

---
+++ apps/backend/routes/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const router = express.Router();
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Import all route files here.
+| Example:
+|
+| const authRoutes = require("./auth.routes");
+| router.use("/auth", authRoutes);
+|
+*/
+
+module.exports = router;
