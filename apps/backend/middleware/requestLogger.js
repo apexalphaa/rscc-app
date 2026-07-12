d75652d3952ea39bd58bdf66a5f61b76b982b9a1
Ankit Sharma <162314034+apexalphaa@users.noreply.github.com>
2026-07-12 20:23:08 +0530

    Create requestLogger.js

---
+++ apps/backend/middleware/requestLogger.js
@@ -0,0 +1,15 @@
+const logger = require("../config/logger");
+
+module.exports = (req, res, next) => {
+  const start = Date.now();
+
+  res.on("finish", () => {
+    const duration = Date.now() - start;
+
+    logger.info(
+      `${req.method} ${req.originalUrl} ${res.statusCode} ${duration}ms`
+    );
+  });
+
+  next();
+};
